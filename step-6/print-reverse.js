3b065d61298ed7a5a41458500dc9bfa3a5344f5f
Theresa Campbell <t.campbell@sasktel.net>
2018-06-11 18:14:38 +0000

    made a function to reverse the html

--- step-6/print-reverse.js
+++ step-6/print-reverse.js
@@ -1,1 +1,12 @@
 var getHTML = require("../step-5-export");
+
+var requestOptions = {
+  host: "sytantris.github.io",
+  path: "/http-examples/step6/reverse.html"
+};
+
+function printReverse (html) {
+  console.log(html.split("").reverse().join(""));
+}
+
+getHTML(requestOptions, printReverse);
